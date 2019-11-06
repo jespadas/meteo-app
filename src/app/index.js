10362f51062c3f327c91fdd8d63fcec98521e9bd
Julio Espadas <julio.espadas@gmail.com>
2019-11-06 16:44:05 +0100

    render data from api done

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,13 @@
 const { Weather } = require('./Weather');
+const { UI } = require('./UI');
+
+const ui = new UI();
 const weather = new Weather('Toulouse', 'fr');
 require('./index.css');
 
 async function fetchWeather() {
   const data = await weather.getWeather();
+  ui.render(data);
   console.log(data);
 }
 
