df270ef2503159ade2a851568ec29810c4d135af
Julio Espadas <julio.espadas@gmail.com>
2019-11-13 20:23:03 +0100

    icons added in display

--- src/app/UI.js
+++ src/app/UI.js
@@ -2,6 +2,8 @@ export class UI {
 
   constructor() {
     this.location = document.getElementById('weather-location');
+    this.flag = document.getElementById('weather-flag');
+    this.icon = document.getElementById('weather-icon');
     this.desc = document.getElementById('weather-description');
     this.string = document.getElementById('weather-string');
     this.humidity = document.getElementById('weather-humidity');
@@ -9,7 +11,9 @@ export class UI {
   }
 
   render(weather) {
-    this.location.textContent = weather.name + '/'+ weather.sys.country;
+    this.flag.setAttribute('src', 'https://www.countryflags.io/' + weather.sys.country + '/flat/64.png');
+    this.location.textContent = weather.name + ','+ weather.sys.country;
+    this.icon.setAttribute('src', "/src/img/" + weather.weather[0]['icon'] + ".png");
     this.desc.textContent = weather.weather[0]['description'];
     this.string.textContent = weather.main.temp + ' °C' ;
     this.humidity.textContent = 'Humidity: ' + weather.main.humidity + '%';
